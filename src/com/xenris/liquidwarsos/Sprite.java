aca0a10eb69319da6a71b89799aec942b847f31b
Henry Shepperd <hshepperd@gmail.com>
2014-07-31 21:12:02 +1000

    Add 2d opengl sprite

---
+++ src/com/xenris/liquidwarsos/Sprite.java
@@ -0,0 +1,99 @@
+//    This file is part of Liquid Wars.
+//
+//    Copyright (C) 2013 Henry Shepperd (hshepperd@gmail.com)
+//
+//    Liquid Wars is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Liquid Wars is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Liquid Wars.  If not, see <http://www.gnu.org/licenses/>.
+
+package com.xenris.liquidwarsos;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLUtils;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import javax.microedition.khronos.opengles.GL10;
+
+public class Sprite {
+    private FloatBuffer vertexBuffer;
+    private float vertices[] = {
+        -1.0f, -1.0f,  0.0f,
+        -1.0f,  1.0f,  0.0f,
+         1.0f, -1.0f,  0.0f,
+         1.0f,  1.0f,  0.0f
+    };
+
+    private FloatBuffer textureBuffer;
+    private float texture[] = {
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        1.0f, 0.0f
+    };
+
+    private int[] textures = new int[1];
+
+    private float gScaleX = 1;
+    private float gScaleY = 1;
+
+    public Sprite(GL10 gl, Context context, int resourceId) {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        vertexBuffer = byteBuffer.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
+
+        byteBuffer = ByteBuffer.allocateDirect(texture.length * 4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        textureBuffer = byteBuffer.asFloatBuffer();
+        textureBuffer.put(texture);
+        textureBuffer.position(0);
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resourceId);
+
+        gl.glGenTextures(1, textures, 0);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+
+        bitmap.recycle();
+    }
+
+    public void draw(GL10 gl, float x, float y) {
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+
+        gl.glTranslatef(x, y, 0);
+        gl.glScalef(gScaleX, gScaleY, 1);
+
+        gl.glFrontFace(GL10.GL_CW);
+
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+    }
+
+    public void scale(float x, float y) {
+        gScaleX = x;
+        gScaleY = y;
+    }
+}
